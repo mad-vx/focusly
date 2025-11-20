11d1518dedc386777444d1751fa171148501cb4c
Mark Evans <mn8s@hotmail.com>
2025-11-20 16:07:04 +0000

    Tidy app.config

--- focusly/projects/focusly-demo-app/src/app/app.config.ts
+++ focusly/projects/focusly-demo-app/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import {
   ApplicationConfig,
-  provideZoneChangeDetection,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -8,7 +7,6 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    // provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimations(),
   ],
 };
