eb965d301fd55e94d75eff2ab25e2df01e985437
Mark Evans <mn8s@hotmail.com>
2025-12-01 10:03:00 +0000

    provideFocuslyKeymap helper method

--- focusly/projects/focusly-demo-app/src/app/app.config.ts
+++ focusly/projects/focusly-demo-app/src/app/app.config.ts
@@ -1,18 +1,17 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideFocuslyKeymap } from '@zaybu/focusly';
 import { FOCUSLY_KEYMAP } from '@zaybu/focusly';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZonelessChangeDetection(), 
     provideAnimations(),
+    provideFocuslyKeymap(
     {
-      provide: FOCUSLY_KEYMAP,
-      useValue: {
         'down': 'ctrl+arrowdown',
         'up': 'ctrl+arrowup',
         'left': 'ctrl+arrowleft',
         'right': 'ctrl+arrowright'
-      }
-    }
+    })
   ],
 };
